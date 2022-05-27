1ff063d6719fce35572251e082ae6bf169efc7f9
Cody <50862704+kody435@users.noreply.github.com>
2022-05-27 23:32:59 +0530

    Add files via upload

---
+++ pages/Posts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Posts() {
+  return (
+    <div>Please head to home to see posts</div>
+  )
+}
+
+export default Posts 
---
+++ pages/posts/[slug].js
@@ -0,0 +1,56 @@
+import { GraphQLClient, gql } from "graphql-request";
+import styles from "../../styles/Home.module.css";
+
+const graphcms = new GraphQLClient(
+  "https://api-eu-central-1.graphcms.com/v2/cl3ncltbk3tw301xkeczu1puc/master"
+);
+
+const QUERY = gql`
+  query Post ($slug: String!){
+    post(where: { slug: $slug }) {
+      title
+      slug
+      url
+    }
+  }
+`;
+const SLUGLIST = gql`
+  {
+    posts {
+      slug
+    }
+  }
+`;
+
+export async function getStaticPaths() {
+  const { posts } = await graphcms.request(SLUGLIST);
+  return {
+    paths: posts.map((post) => ({ params: { slug: post.slug } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const slug = params.slug;
+  const data = await graphcms.request(QUERY, { slug });
+  const post = data.post;
+  return {
+    props: {
+      post,
+    },
+    revalidate: 30,
+  };
+}
+
+export default function BlogPost({ post }) {
+  return (
+    <main className={styles.main}>
+      <div>
+        <iframe id="iframe" src={post.url} width="100%" height="100%" allowFullScreen className={styles.iframe}></iframe>
+        <div className={styles.divi}>
+          <h1 className={styles.h2}>{post.title}</h1>
+        </div>
+      </div>
+    </main>
+  );
+}

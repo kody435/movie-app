fddb59a30f2ccda38ea210e22e226f8c76d09df1
Cody <bittusmaths@gmail.com>
2022-09-04 16:48:23 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,45 @@
 import '../styles/globals.css'
+import Script from "next/script"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
 
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7866444855575414"
+        crossorigin="anonymous"
+      ></script>
+
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-7866444855575414"
+        async
+        strategy="afterInteractive"
+        onError={(e) => {
+          console.error("Script failed to load", e);
+        }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
+}
 export default MyApp
